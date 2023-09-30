058e90cfbdc0466c22ffdb788a38b451f7fadb2a
Dinesh <dinesh_sripathi_panditharadhyula@epam.com>
2023-09-30 13:10:56 +0530

    Add method

---
+++ demo/src/main/java/com/epam/demo/Add.java
@@ -0,0 +1,14 @@
+package com.epam.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Add {
+
+    public int add(int a, int b)
+    {
+        return a+b;
+    }
+
+
+}
