49c2d678acf699f15262b463025dda28404370bb
Dinesh <dinesh_sripathi_panditharadhyula@epam.com>
2023-09-30 13:58:26 +0530

    Add method

--- demo/src/main/java/com/epam/demo/DemoApplication.java
+++ demo/src/main/java/com/epam/demo/DemoApplication.java
@@ -1,16 +1,28 @@
 package com.epam.demo;
 
+import lombok.experimental.Accessors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
+@Configuration
+@ComponentScan(basePackages = {"com.epam.demo"})
 public class DemoApplication {
-
+	private static Add add;
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.print("hello");
+		ApplicationContext context = new AnnotationConfigApplicationContext(DemoApplication.class);
+		add = context.getBean(Add.class);
+				//AppContext.getApplicationContext().getBean(VaccineManagementTool.class);
+
+		System.out.print("hello"+add.add(1,3));
 	}
 
 }
